7c4480b2109b28185b097ea6c5b1cd4bdf258009
Abhijit Sarkar <contact@asarkar.com>
2022-07-11 03:01:00 -0700

    Add more comments to perfect-numbers

--- perfect-numbers/src/lib.rs
+++ perfect-numbers/src/lib.rs
@@ -8,9 +8,10 @@ pub enum Classification {
 }
 
 /*
- * It can be shown that the sum of the divisors of a
- * number can be expressed at the sum of some powers
+ * It can be shown that the sum of the divisors of a number
+ * can be expressed as the product of the sum of the powers
  * of the prime factors of the number.
+ *
  * Example:
  * n = 18, sum of divisors = 1 + 2 + 3 + 6 + 9 + 18
  * = 2^0 x 3^0 + 2^1 x 3^0 + 2^0 x 3^1 +
@@ -19,7 +20,16 @@ pub enum Classification {
  * = (1 + p1) x (1 + p2 + p2^2), where p1=2, and p2=3
  *
  * So, the task reduces to finding all the prime factors
- * and the sum of their powers.
+ * and the product of the sum of their powers.
+ *
+ * Furthermore, the highest power of a factor is the
+ * number of times it divides the "remaining" number.
+ * "Remaining" means the result after progressively
+ * dividing the original number with the smaller prime
+ * factors.
+ *
+ * In the example above, the highest power of 2 is 1.
+ * The highest power of 3 is 2.
  */
 pub fn classify(num: u64) -> Option<Classification> {
     if num == 0 {
@@ -43,9 +53,13 @@ pub fn classify(num: u64) -> Option<Classification> {
         }
         sum *= curr_sum;
     }
-    // This condition is to handle the
-    // case when n is a prime number
-    // greater than 2
+    /*
+     * This condition is to handle the case when remaining
+     * n is a prime number greater than 2.
+     *
+     * For example, when original n = 6, sqrt = 2, and
+     * remaining n = 3.
+     */
     if n > 2 {
         sum *= 1 + n;
     }
